7f84a61421f93f67422a41f6e62ecc498cc5f5b8
luizeduardodjs <luiz.santana059@academico.ifs.edu.br>
2021-08-04 01:42:02 -0300

    Preparando projeto de acesso a dados

---
+++ src/Api.ConfTerm.Data/Contexts/MeasurementContext.cs
@@ -0,0 +1,30 @@
+﻿using Api.ConfTerm.Data.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.ConfTerm.Data.Contexts
+{
+    public class MeasurementContext : DbContext
+    {
+        public MeasurementContext(DbContextOptions<MeasurementContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                // optionsBuilder.UseSqlServer(@"Server=.\SQLExpress;Database=SchoolDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .ApplyMappingConfigurations()
+                .ApplyManyToManyMappingConfigurations();
+        }
+    }
+}
---
+++ src/Api.ConfTerm.Data/Extensions/EntityTypeConfigurationExentensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.ConfTerm.Data.Extensions
+{
+    public static class EntityTypeConfigurationExentensions
+    {
+        public static ModelBuilder ApplyMappingConfigurations(this ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new ProductMap());
+            //modelBuilder.ApplyConfiguration(new CategoryMap());
+            //modelBuilder.ApplyConfiguration(new OptionMap());
+            //modelBuilder.ApplyConfiguration(new ItemMap());
+
+            //modelBuilder.ApplyConfiguration(new CustomerMap());
+            //modelBuilder.ApplyConfiguration(new CompanyMap());
+            //modelBuilder.ApplyConfiguration(new ProfileMap());
+            //modelBuilder.ApplyConfiguration(new ProfileHistoricMap());
+            //modelBuilder.ApplyConfiguration(new CompanyTypeMap());
+
+            //modelBuilder.ApplyConfiguration(new AddressMap());
+
+            return modelBuilder;
+        }
+
+        public static ModelBuilder ApplyManyToManyMappingConfigurations(this ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Product>().HasMany(product => product.Options).WithMany(option => option.Products);
+            //modelBuilder.Entity<Company>().HasMany(company => company.Owners).WithMany(customer => customer.Companies);
+            return modelBuilder;
+        }
+    }
+}
