8c6e221261120d2cdaeed34a9193140e961d61ee
luizeduardodjs <luiz.santana059@academico.ifs.edu.br>
2021-08-04 01:41:35 -0300

    Criando leitor agnostico de variaveis de ambiente

---
+++ Api.ConfTerm.Service/General/EnviromentVariableReader.cs
@@ -0,0 +1,10 @@
+﻿using Api.ConfTerm.Core.Services;
+using System;
+
+namespace Api.ConfTerm.Services.General
+{
+    public class EnviromentVariableReader : IEnviromentVariableReader
+    {
+        string IEnviromentVariableReader.DatabaseUrl => Environment.GetEnvironmentVariable("DATABASE_URL");
+    }
+}
