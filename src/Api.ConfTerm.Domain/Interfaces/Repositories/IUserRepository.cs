6e0df74f99b7632997fc27feca6bc44d7f8170dc
ejesus <ejesus@DESKTOP-EFQFQPL>
2021-09-18 00:41:00 -0300

    Usando CancellationToken na camada de acesso a dados

--- src/Api.ConfTerm.Domain/Interfaces/Repositories/IUserRepository.cs
+++ src/Api.ConfTerm.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Api.ConfTerm.Domain.Entities;
 using Api.ConfTerm.Domain.ValueObjects;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Api.ConfTerm.Domain.Interfaces.Repositories
 {
     public interface IUserRepository : IRepository<User>
     {
-        public Task<User> GetByEmailAsync(Email email);
+        public Task<User> GetByEmailAsync(Email email, CancellationToken cancelletionToken = default);
     }
 }
--- src/Api.ConfTerm.Domain/Interfaces/Services/IUnitOfWork.cs
+++ src/Api.ConfTerm.Domain/Interfaces/Services/IUnitOfWork.cs
@@ -6,7 +6,6 @@ namespace Api.ConfTerm.Domain.Interfaces.Services
     // Tagging Interface
     public interface IUnitOfWork
     {
-        public int SaveChanges();
         public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
