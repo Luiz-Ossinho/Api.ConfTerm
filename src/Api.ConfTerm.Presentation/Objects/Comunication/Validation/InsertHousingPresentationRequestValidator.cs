eaee6ccdf8e3c5e30002e5de42708a5f7972de91
ejesus <ejesus@DESKTOP-EFQFQPL>
2021-09-19 18:59:19 -0300

    Criando validacoes de request

---
+++ src/Api.ConfTerm.Presentation/Objects/Comunication/Validation/InsertHousingPresentationRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Api.ConfTerm.Domain.ValueObjects;
+using Api.ConfTerm.Presentation.Objects.Comunication.Requests;
+using FluentValidation;
+
+namespace Api.ConfTerm.Presentation.Objects.Comunication.Validation
+{
+    public class InsertHousingPresentationRequestValidator : AbstractValidator<InsertHousingPresentationRequest>
+    {
+        public InsertHousingPresentationRequestValidator()
+        {
+            RuleFor(r => r.Identificantion)
+                .NotEmpty()
+                .MaximumLength(255);
+
+            RuleFor(r => r.UserEmail)
+                .NotEmpty()
+                .MaximumLength(255)
+                .Must(r => Email.IsValid(new Email(r))).WithMessage(r => $"is not a valid email");
+        }
+    }
+}
