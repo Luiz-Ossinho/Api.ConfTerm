51b2c40460ec4039114545db9673f0430d222721
ejesus <ejesus@DESKTOP-EFQFQPL>
2021-09-19 18:59:00 -0300

    Adicionando AutoMapper

--- src/Api.ConfTerm.Presentation/Controllers/Api/AlojamentoController.cs
+++ src/Api.ConfTerm.Presentation/Controllers/Api/AlojamentoController.cs
@@ -1,5 +1,7 @@
-﻿using Api.ConfTerm.Domain.ValueObjects;
+﻿using Api.ConfTerm.Application.Objects.Requests;
+using Api.ConfTerm.Domain.ValueObjects;
 using Api.ConfTerm.Presentation.Objects.Comunication.Requests;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,13 +12,13 @@ namespace Api.ConfTerm.Presentation.Controllers.Api
 {
     public class AlojamentoController : BaseController
     {
-        public AlojamentoController(IMediator mediator) : base(mediator) { }
+        public AlojamentoController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
 
         [HttpPost]
         [Authorize(Roles = nameof(UserType.Administrator))]
-        public async Task<IActionResult> InsertHousing([FromBody] InserirAlojamentoRequest presentationRequest, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> InsertHousing([FromBody] InsertHousingPresentationRequest presentationRequest, CancellationToken cancellationToken = default)
         {
-            var appRequest = presentationRequest.ToApplicationRequest();
+            var appRequest = _mapper.Map<InsertHousingRequest>(presentationRequest);
             var appResponse = await _mediator.Send(appRequest, cancellationToken);
             return ActionResultOf(appResponse);
         }
--- src/Api.ConfTerm.Presentation/Controllers/Api/ConfortController.cs
+++ src/Api.ConfTerm.Presentation/Controllers/Api/ConfortController.cs
@@ -1,5 +1,7 @@
-﻿using Api.ConfTerm.Domain.ValueObjects;
+﻿using Api.ConfTerm.Application.Objects.Requests;
+using Api.ConfTerm.Domain.ValueObjects;
 using Api.ConfTerm.Presentation.Objects.Comunication.Requests;
+using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,14 +12,14 @@ namespace Api.ConfTerm.Presentation.Controllers.Api
 {
     public class ConfortoController : BaseController
     {
-        public ConfortoController(IMediator mediator) : base(mediator) { }
+        public ConfortoController(IMediator mediator, IMapper mapper) : base(mediator, mapper) { }
 
         [HttpPost]
         [Authorize(Roles = nameof(UserType.Administrator))]
         [Route("Itu")]
-        public async Task<IActionResult> InsertTHIConfort([FromBody] InserirConfortoITURequest presentationRequest, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> InsertTHIConfort([FromBody] InsertTemperatureHumidityIndexConfortPresentationRequest presentationRequest, CancellationToken cancellationToken = default)
         {
-            var appRequest = presentationRequest.ToApplicationRequest();
+            var appRequest = _mapper.Map<InsertTemperatureHumidityIndexConfortRequest>(presentationRequest);
             var appResponse = await _mediator.Send(appRequest, cancellationToken);
             return ActionResultOf(appResponse);
         }
@@ -25,9 +27,9 @@ namespace Api.ConfTerm.Presentation.Controllers.Api
         [HttpPost]
         [Authorize(Roles = nameof(UserType.Administrator))]
         [Route("Itgu")]
-        public async Task<IActionResult> InsertBGTHIConfort([FromBody] InserirConfortoITGURequest presentationRequest, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> InsertBGTHIConfort([FromBody] InsertBlackGlobeTemparuteHumidityIndexConfortPresentationRequest presentationRequest, CancellationToken cancellationToken = default)
         {
-            var appRequest = presentationRequest.ToApplicationRequest();
+            var appRequest = _mapper.Map<InsertBlackGlobeTemparuteHumidityIndexConfortRequest>(presentationRequest);
             var appResponse = await _mediator.Send(appRequest, cancellationToken);
             return ActionResultOf(appResponse);
         }
@@ -35,9 +37,9 @@ namespace Api.ConfTerm.Presentation.Controllers.Api
         [HttpPost]
         [Authorize(Roles = nameof(UserType.Administrator))]
         [Route("TemperaturaUmidade")]
-        public async Task<IActionResult> InsertTHConfort([FromBody] InserirConfortoTemperaturaUmidadeRequest presentationRequest, CancellationToken cancellationToken = default)
+        public async Task<IActionResult> InsertTHConfort([FromBody] InsertTemperatureHumidityConfortPresentationRequest presentationRequest, CancellationToken cancellationToken = default)
         {
-            var appRequest = presentationRequest.ToApplicationRequest();
+            var appRequest = _mapper.Map<InsertTemperatureHumidityConfortRequest>(presentationRequest);
             var appResponse = await _mediator.Send(appRequest, cancellationToken);
             return ActionResultOf(appResponse);
         }
